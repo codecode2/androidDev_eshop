ab800fbabf050b601ec5c9c67b636d4bb58c1416
code <al_kis93@hotmail.com>
2023-04-08 00:49:45 +0300

    edit username and password

--- app/src/main/java/com/example/eshop/MainActivity.java
+++ app/src/main/java/com/example/eshop/MainActivity.java
@@ -2,26 +2,45 @@ package com.example.eshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
+    private sharedPreferenceConfig sharedPreferenceConfig;
+
+    EditText UserName, UserPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferenceConfig = new sharedPreferenceConfig(getApplicationContext());
+        UserName = findViewById(R.id.nameLogin);
+        UserPassword = findViewById(R.id.loginPassword);
+        if (sharedPreferenceConfig.readLoginStatus()){
+            startActivity(new Intent(this, WelcomePage.class));
+            finish();
+        }
     }
 
 
-    public void changeActivity(View view) {
-
-
-        Intent intent = new Intent(this, WelcomePage.class);
-
-        startActivity(intent);
+    public void loginUser(View view) {
+        String varusername = UserName.getText().toString();
+        String varuserpassword = UserPassword.getText().toString();
+        if (varusername.equals(getResources().getString(R.string.user_name))&& varuserpassword.equals((getResources().getString(R.string.user_password)))){
+            startActivity(new Intent(this, WelcomePage.class));
+            sharedPreferenceConfig.writeLoginStatus(true);
+            finish();
+        } else {
+            Toast.makeText(this, "Login failed! Try again ...", Toast.LENGTH_LONG).show();
+            UserName.setText("");
+            UserPassword.setText("");
+        }
     }
 }
 
--- app/src/main/java/com/example/eshop/WelcomePage.java
+++ app/src/main/java/com/example/eshop/WelcomePage.java
@@ -11,11 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomePage extends AppCompatActivity {
 
+    private sharedPreferenceConfig sharedPreferenceConfig;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
+        sharedPreferenceConfig = new sharedPreferenceConfig(getApplicationContext());
      }
+
+    public void logoutUser(View view) {
+        sharedPreferenceConfig.writeLoginStatus(false);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+        }
+
+
     }
 
 
